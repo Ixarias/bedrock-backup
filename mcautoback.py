6823d0ba8f071061ab6c8964f02d1ece70089061
Ixarias <lacareo@gmail.com>
2020-09-03 10:56:02 -0500

    Failsafe additions
    Now catches if the backup fails when Minecraft is running, and includes the option to run as admin

--- mcautoback.py
+++ mcautoback.py
@@ -1,3 +1,4 @@
+import ctypes, sys
 import shutil
 import os
 from os import path
@@ -5,8 +6,18 @@ from datetime import datetime
 from time import sleep
 from glob import glob
 
-#// Define time between backups, in seconds
+#// Define time between backups, in seconds (Default: 300)
 WaitTime = 300
+#// Reduces wait time in case of backup failure by a factor of FailMod (Default: 5)
+FailMod = 5
+#// Wether to require admin permissions
+RunAsAdmin = 0
+
+def is_admin():
+    try:
+        return ctypes.windll.shell32.IsUserAnAdmin()
+    except:
+        return False
 
 def bedrockpathtostring(worldname):
     worldpath = path.expandvars('%localappdata%/Packages/Microsoft.MinecraftUWP_8wekyb3d8bbwe/LocalState/games/com.mojang/minecraftWorlds')
@@ -27,10 +38,18 @@ def backup():
     print(f'Backing up world folder: {bedrockname} ({worldname})')
 
     while True:
-        shutil.make_archive(backuppath, 'zip', bedrockpath)
-        print(f"New Backup made at: {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}")
-        print("Waiting...")
-        sleep(WaitTime)
+        try:
+            shutil.make_archive(backuppath, 'zip', bedrockpath)
+            print(f"New Backup made at: {datetime.now().strftime('%Y-%m-%d %H:%M:%S')}")
+            print('Waiting' + str(WaitTime) + seconds)
+            sleep(WaitTime)
+        except:
+            print('Backup unavailable while the Minecraft World is running')
+            print('Trying again in ' + str(WaitTime/FailMod) + ' seconds')
+            sleep(WaitTime/FailMod)
         
 if __name__ == '__main__':
+    if RunAsAdmin == 1 and not is_admin():
+        #re-run as admin
+        ctypes.windll.shell32.ShellExecuteW(None, "runas", sys.executable, " ".join(sys.argv), None, 1)
     backup()

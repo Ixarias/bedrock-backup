eaffca3352e4219a5f67e20a322efb02fc1b8852
Ixarias <lacareo@gmail.com>
2020-11-05 10:02:01 -0600

    move sleep to async
    detaches backup process from sleep async call

--- mcautoback.py
+++ mcautoback.py
@@ -6,8 +6,8 @@ from datetime import datetime
 from time import sleep
 from glob import glob
 from dearpygui import core, simple
-from dearpygui.core import add_text, get_value, log_debug, run_async_function, start_dearpygui
-from dearpygui.simple import show_about, show_documentation
+from dearpygui.core import add_text, get_value, log_debug, run_async_function, show_logger, start_dearpygui
+from dearpygui.simple import show_about, show_debug, show_documentation
 
 def is_admin():
     try:
@@ -35,38 +35,40 @@ def findworld(worldname):
     bedrockname = bedrockpath[119:]
     return backuppath, bedrockpath, bedrockname
 
-def dobackup(backuppath, bedrockpath, WaitTime, FailMod):
-    while True:
+def dobackup(backuppath, bedrockpath):
         try:
             fullpath = backuppath + str(datetime.now().strftime('[%Y_%m_%d]_[%H_%M]'))
             shutil.make_archive(fullpath, 'zip', bedrockpath)
-            # log_debug(f"New Backup made at: {datetime.now().strftime('%Y-%m-%d %H:%M')}")
-            # log_debug('Waiting ' + str(WaitTime) + ' seconds')
-            sleep(WaitTime)
+            os.rename(fullpath + '.zip', fullpath + '.mcworld')
+            log_debug(f"New Backup made at: {datetime.now().strftime('%Y-%m-%d %H:%M')}")
+            return 1
         except KeyboardInterrupt:
-            sys.exit()
+            return 0
         except:
-            # log_debug('Backup unavailable while the Minecraft World is running')
-            # log_debug('Trying again in ' + str(WaitTime/FailMod) + ' seconds')
-            sleep(FailMod)
+            return 0
 
-def backupprep(sender, data):
-    backupinfo = (get_value("World Name"), get_value("Wait Time"), get_value("Failure Wait Time"))
-    add_text(f"Backup created at {datetime.now().strftime('[%Y_%m_%d]_[%H_%M]')}", parent="MCAutoBack")
-    run_async_function(backupcallback, backupinfo, return_handler=backupprep)
+def async_sleep(sender, sleeptime):
+    sleep(sleeptime)
 
 def backupcallback(sender, data):
-    worldinfo = findworld(data[0])
-    dobackup(worldinfo[0], worldinfo[1], data[1], data[2])
-
+    backupinfo = (get_value("World Name"), get_value("Wait Time"), get_value("Failure Wait Time"))
+    worldinfo = findworld(backupinfo[0])
+    if (dobackup(worldinfo[0], worldinfo[1]) == 1):
+        add_text(f"Backup created at {datetime.now().strftime('[%Y_%m_%d]_[%H_%M]')}", parent="MCAutoBack")
+        run_async_function(async_sleep, backupinfo[1], return_handler=backupcallback)
+    else:
+        add_text(f'Backup failed. Is the world being accessed?', parent="MCAutoBack")
+        run_async_function(async_sleep, backupinfo[2], return_handler=backupcallback)
+    
 if __name__ == '__main__':
     core.set_main_window_size(600, 200)
 
     with simple.window("MCAutoBack"):
         core.add_text("MCAutoBack")
         core.add_input_text("World Name", default_value="Cube World")
-        core.add_button("Backup", callback=backupprep)
+        core.add_button("Backup", callback=backupcallback)
         core.add_slider_int("Wait Time", default_value=300, min_value=30, max_value=1200)
         core.add_slider_int("Failure Wait Time", default_value=30, min_value=5, max_value=300)
 
+    show_logger()
     start_dearpygui(primary_window="MCAutoBack")

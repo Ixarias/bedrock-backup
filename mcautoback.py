cfdee5d5e939ef483743b5ffee770cc868e2b035
Ixarias <lacareo@gmail.com>
2020-11-05 12:59:58 -0600

    refactor imports to remove unneccesary dependencies

--- mcautoback.py
+++ mcautoback.py
@@ -1,24 +1,15 @@
-import ctypes, sys
 import shutil
-import os
-from os import path
+from os import path, rename, walk
 from datetime import datetime
 from time import sleep
 from glob import glob
-from dearpygui import core, simple
-from dearpygui.core import add_text, get_value, log_debug, run_async_function, show_logger, start_dearpygui
-from dearpygui.simple import show_about, show_debug, show_documentation
-
-def is_admin():
-    try:
-        return ctypes.windll.shell32.IsUserAnAdmin()
-    except:
-        return False
+from dearpygui.core import add_text, add_input_text, add_button, add_slider_int, get_value, log_debug, run_async_function, set_main_window_size, start_dearpygui
+from dearpygui.simple import window
 
 def bedrockpathtostr(worldname):
     worldpath = path.expandvars('%localappdata%/Packages/Microsoft.MinecraftUWP_8wekyb3d8bbwe/LocalState/games/com.mojang/minecraftWorlds')
 
-    result = [y for x in os.walk(worldpath) for y in glob(os.path.join(x[0], '*.txt'))]
+    result = [y for x in walk(worldpath) for y in glob(path.join(x[0], '*.txt'))]
     for x in result:
         txtfile = open(x)
         if txtfile.readline() == worldname:
@@ -39,7 +30,7 @@ def dobackup(backuppath, bedrockpath):
         try:
             fullpath = backuppath + str(datetime.now().strftime('[%Y_%m_%d]_[%H_%M]'))
             shutil.make_archive(fullpath, 'zip', bedrockpath)
-            os.rename(fullpath + '.zip', fullpath + '.mcworld')
+            rename(fullpath + '.zip', fullpath + '.mcworld')
             log_debug(f"New Backup made at: {datetime.now().strftime('%Y-%m-%d %H:%M')}")
             return 1
         except KeyboardInterrupt:
@@ -61,13 +52,13 @@ def backupcallback(sender, data):
         run_async_function(async_sleep, backupinfo[2], return_handler=backupcallback)
     
 if __name__ == '__main__':
-    core.set_main_window_size(480, 400)
+    set_main_window_size(480, 400)
 
-    with simple.window("MCAutoBack"):
-        core.add_text("MCAutoBack")
-        core.add_input_text("World Name", default_value="Cube World")
-        core.add_button("Backup", callback=backupcallback)
-        core.add_slider_int("Wait Time", default_value=300, min_value=30, max_value=1200)
-        core.add_slider_int("Fail Wait Time", default_value=30, min_value=5, max_value=300)
+    with window("MCAutoBack"):
+        add_text("MCAutoBack")
+        add_input_text("World Name", default_value="Cube World")
+        add_button("Backup", callback=backupcallback)
+        add_slider_int("Wait Time", default_value=300, min_value=30, max_value=1200)
+        add_slider_int("Fail Wait Time", default_value=30, min_value=5, max_value=300)
 
     start_dearpygui(primary_window="MCAutoBack")
